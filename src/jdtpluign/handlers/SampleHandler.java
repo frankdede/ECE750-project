7e6b080998c2d01d45e9ce5ab3d2fad254b7437b
Frank Huang <frank@quartic.ai>
2025-02-17 02:41:32 -0500

    Detect and print throw statements invoked in finally block recursively

--- src/jdtpluign/handlers/SampleHandler.java
+++ src/jdtpluign/handlers/SampleHandler.java
@@ -1,8 +1,5 @@
 package jdtpluign.handlers;
 
-import java.awt.print.Printable;
-import java.lang.reflect.Method;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -11,7 +8,6 @@ import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.resources.IWorkspace.ProjectOrder;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragment;
@@ -21,12 +17,6 @@ import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.internal.core.search.matching.MatchLocatorParser.ClassAndMethodDeclarationVisitor;
-import org.eclipse.jdt.launching.sourcelookup.containers.JavaProjectSourceContainer;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.osgi.service.resolver.ImportPackageSpecification;
 
 public class SampleHandler extends AbstractHandler {
 
@@ -106,12 +96,11 @@ public class SampleHandler extends AbstractHandler {
 		
 //		MethodDeclarationVisitor visitor = new MethodDeclarationVisitor();
 //		CatchVisitor visitor = new CatchVisitor();
+
 		TryVisitor visitor = new TryVisitor();
+		
+		System.out.println("Started analyzing...");
 		astRoot.accept(visitor);
-//		System.out.println(visitor.getCatchCount());
-		
-		
-//		System.out.println("Number of methods visitor:" + Integer.toString(visitor.getMethodCount()));
 	}
 	
 }
